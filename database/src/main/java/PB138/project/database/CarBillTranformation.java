4b739575ff8f664daa1c8870278b20bef2db3d7c
Matej <matejkralik@hotmail.com>
2015-05-21 23:35:39 +0200

    Add path

--- database/src/main/java/PB138/project/database/CarBillTranformation.java
+++ database/src/main/java/PB138/project/database/CarBillTranformation.java
@@ -1,30 +1,30 @@
 package PB138.project.database;
 
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.*;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import java.io.File;
+import java.net.URL;
 
 /**
  * Created by Michal on 15.5.2015.
  */
 public class CarBillTranformation {
 
-    public static void makeBill()
-            throws TransformerConfigurationException, TransformerException {
+    public static void makeBill(String path)
+            throws  TransformerException {
 
         TransformerFactory tf = TransformerFactory.newInstance();
 
         //System.out.println(tf.getClass());
-
+        System.out.println("ASFADGSD "+path+"WEB-INF\\classes\\xslt\\bill_of_sale.xsl");
         Transformer xsltProc = tf.newTransformer(
-                new StreamSource(new File("src/main/resources/xslt/bill_of_sale.xsl")));
+                new StreamSource(new File(path+"WEB-INF\\classes\\xslt\\bill_of_sale.xsl")));
+
+        xsltProc.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
 
         xsltProc.transform(
-                new StreamSource(new File("src/main/resources/carForSale.xml")),
-                new StreamResult(new File("src/main/resources/carBill.html")));
+                new StreamSource(new File(path+"WEB-INF\\classes\\tmp\\contract.xml")),
+                new StreamResult(new File(path+"car.html")));
     }
 }
